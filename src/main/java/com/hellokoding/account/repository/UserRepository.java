4e57a43466b528b83486d6eda312bc0230a7cc3d
jayaramakrishnagadugoyya <83549040+jayaramakrishnagadugoyya@users.noreply.github.com>
2021-12-01 20:14:36 +0530

    Update UserRepository.java

--- src/main/java/com/hellokoding/account/repository/UserRepository.java
+++ src/main/java/com/hellokoding/account/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.hellokoding.account.repository;
-package com.hellokoding2.account.repository:
+
+
 import com.hellokoding.account.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
